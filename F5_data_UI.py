77ae98f2290d4fb50210bc9f3a8d487d17ebaec6
Marek Janda <Marek.Janda@jehall.co.uk>
2020-05-18 16:23:17 +0100

    Working GUI, without possibility to select report parameters

--- F5_data_UI.py
+++ F5_data_UI.py
@@ -4,11 +4,12 @@ import errno
 # importing tkinter and tkinter.ttk 
 # and all their functions and classes 
 from tkinter import * 
+from tkinter import messagebox
 from tkinter.ttk import *
   
 # importing askopenfile function 
 # from class filedialog 
-from tkinter.filedialog import askopenfilename 
+from tkinter.filedialog import askopenfilename, askdirectory 
 
 import pandas as pd
 #import numpy as np
@@ -18,40 +19,55 @@ from datetime import date
 
 print("Libraries imported\n")
 
+# Set entries and labels
 root = Tk() 
 ent1 = Entry(root, font=40)
-ent1.grid(row=2,column=2)
-ent2 = Entry(root,font=40)
-ent2.grid(row=4,column=2)
+ent1.grid(row=1, column=2)
+label1 = Label(root, text="Test data: ")
+label1.grid(row=1, column=1, padx=10, pady=10)
+ent2 = Entry(root, font=40)
+ent2.grid(row=2, column=2)
+label2 = Label(root, text="Logsheet: ")
+label2.grid(row=2, column=1, padx=10, pady=10)
+ent3 = Entry(root, font=40)
+ent3.grid(row=3, column=2)
+label3 = Label(root, text="Report destination: ")
+label3.grid(row=3, column=1, padx=10, pady=10)
 
 # Set relative path to test log and logsheet
 #xls_path = "LOGall-fullycond.xls"
 #logsheet = "Logsheet_PR_curve.txt"
 
 # Set headers and intialize an empty data frame
-headers = ["Conditions", "Logs", "f [Hz]", "PR", "VR", f"SG [{chr(176)}C]", f"DG [{chr(176)}C]", f"SSH [{chr(176)}C]", "Duty [kW]", "Flow rate [m3/h]", "IE [%]", "VE [%]", "COP [-]"]
-output_df = pd.DataFrame(columns = headers)
+#headers = ["Conditions", "Logs", "f [Hz]", "PR", "VR", f"SG [{chr(176)}C]", f"DG [{chr(176)}C]", f"SSH [{chr(176)}C]", "Duty [kW]", "Flow rate [m3/h]", "IE [%]", "VE [%]", "COP [-]"]
+#output_df = pd.DataFrame(columns = headers)
 
-# Compile test report name based on date
-today = date.today()
-day = today.strftime("%d%m%y")
-output_file_name = f"test_report_{day}"
-print(output_file_name)
-
-# Set the output file directory
-test_report = f"reports\{output_file_name}.xlsx"
-if not os.path.exists(os.path.dirname(test_report)):
-    try:
-        os.makedirs(os.path.dirname(test_report))
-    except OSError as exc: # Guard against race condition
-        if exc.errno != errno.EEXIST:
-            raise
+def select_report_destination():
+   # First delete exsiting entry (if there is existing entry)
+   ent3.delete(0, END)
+   # Compile test report name based on date
+   today = date.today()
+   day = today.strftime("%d%m%y")
+   output_file_name = f"test_report_{day}"
+   print(output_file_name)
+   report_dest = askdirectory()
+   # Set the output file directory
+   test_report = f"{report_dest}\{output_file_name}.xlsx"
+   print(f"Destination fodler: {test_report}")
+   if not os.path.exists(os.path.dirname(test_report)):
+      try:
+         os.makedirs(os.path.dirname(test_report))
+      except OSError as exc: # Guard against race condition
+         if exc.errno != errno.EEXIST:
+               raise
+   ent3.insert(END, test_report)
 
 # Read the log file
 #results = pd.read_excel(xls_path)
 global results
 
 def open_results(): 
+   ent1.delete(0, END)
    global results_sheet
    results_sheet = askopenfilename(filetypes =[("all files","*.*")]) 
    if results_sheet is not None:       
@@ -63,13 +79,16 @@ def open_results():
 #print(results.head())
 
 def open_logsheet():
+   ent2.delete(0, END)
    logfile = askopenfilename(filetypes =[("all files","*.*")])
    with open(logfile,'r') as testlog:
       ent2.insert(END, logfile)
 
 def make_report():
+   # Get entries
    test_results = ent1.get()
    logsheet = ent2.get()
+   test_report = ent3.get()
    # Set headers and intialize an empty data frame
    headers = ["Conditions", "Logs", "f [Hz]", "PR", "VR", f"SG [{chr(176)}C]", f"DG [{chr(176)}C]", f"SSH [{chr(176)}C]", "Duty [kW]", "Flow rate [m3/h]", "IE [%]", "VE [%]", "COP [-]"]
    output_df = pd.DataFrame(columns = headers)
@@ -128,20 +147,44 @@ def make_report():
 
    with pd.ExcelWriter(test_report) as writer:    
       # Write the dataframe into test report excel file
-      output_df.to_excel(writer, sheet_name='Summary', startrow=1, startcol=1)
+      output_df.to_excel(writer, sheet_name='Summary', startrow=1, startcol=1, index=False)
+      #summary_sheet = writer.sheets['Summary']
+      #summary_sheet.set_column('B:O', 20)
       # Add test log to the report
-      results.to_excel(writer, sheet_name='Test_log', startrow=0, startcol=0)
-   print("Writing finished")      
+      results.to_excel(writer, sheet_name='Test_log', startrow=0, startcol=0, index=False)
+      #data_sheet = writer.sheets['Test_log']
+      #data_sheet.set_column('A:', 40)
+   messagebox.showinfo("Success", f"Report successfuly generated at: {test_report}")
+
+# Close function
+def close_window(): 
+    root.destroy()
+
+# Define button styling
+style = Style()
+style.map("C.TButton",
+    foreground=[('pressed', 'red'), ('active', 'blue')],
+    background=[('pressed', '!disabled', 'black'), ('active', 'white')]
+    ) 
 
 # Define button to select test data
-test_data_btn = Button(root, text ='Select test data', command = lambda:open_results()) 
-test_data_btn.grid(row=1,column=2)
+test_data_btn = Button(root, text ='Select test data', style="C.TButton", command = lambda:open_results()) 
+test_data_btn.grid(row=1,column=3, padx=10, pady=10)
 
 # Define button to select log sheet
-logsheet_btn = Button(root, text ='Select Logsheet', command = lambda:open_logsheet())
-logsheet_btn.grid(row=3,column=2)
+logsheet_btn = Button(root, text ='Select Logsheet', style="C.TButton", command = lambda:open_logsheet())
+logsheet_btn.grid(row=2,column=3, padx=10, pady=10)
 
-make_btn = Button(root, text ='Make report', command = lambda:make_report())
-make_btn.grid(row=5,column=2)
+# Define button to select report destination folder
+report_dest_btn = Button(root, text='Browse', style="C.TButton", command = lambda:select_report_destination())
+report_dest_btn.grid(row=3, column=3, padx=10, pady=10)
+
+# Define button to generate the report
+make_btn = Button(root, text ='Make report', style="C.TButton", command = lambda:make_report())
+make_btn.grid(row=5,column=1, padx=10, pady=10)
+
+# Define button to close/terminate the program
+cancel_btn = Button(root, text="Close", style="C.TButton", command = lambda:close_window())
+cancel_btn.grid(row=5, column=3, padx=10, pady=10)
 
 mainloop() 

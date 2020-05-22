f796953ba16923c294dbb4372c4966875f9bb323
Marek Janda <Marek.Janda@jehall.co.uk>
2020-05-22 15:54:38 +0100

    Added Yellow rig data processing with GUI selection

--- F5_data_GUI.py
+++ F5_data_GUI.py
@@ -33,6 +33,12 @@ ent3 = Entry(root, font=40)
 ent3.grid(row=3, column=2)
 label3 = Label(root, text="Report destination: ")
 label3.grid(row=3, column=1, padx=10, pady=10)
+# Rig selection
+rig = StringVar()
+R1 = Radiobutton(root, text="F5 Rig", variable=rig, value="F5")
+R1.grid(row=4, column=1, padx=10, pady=10)
+R2 = Radiobutton(root, text="Yellow Rig", variable=rig, value="yellow")
+R2.grid(row=4, column=2, padx=10, pady=10)
 
 # Set relative path to test log and logsheet
 #xls_path = "LOGall-fullycond.xls"
@@ -84,7 +90,7 @@ def open_logsheet():
    with open(logfile,'r') as testlog:
       ent2.insert(END, logfile)
 
-def make_report():
+def make_F5_report():
    # Get entries
    test_results = ent1.get()
    logsheet = ent2.get()
@@ -110,7 +116,7 @@ def make_report():
             averaged_results["VR"] = results.loc[19, log]
             averaged_results["Duty"] += results.loc[249, log]
             averaged_results["Flow rate"] += results.loc[240, log]
-            averaged_results["SG"] += results.loc[13, log]
+            averaged_results["SG"] += results.loc[30, log]
             averaged_results["DG"] += results.loc[34, log]
             averaged_results["SSH"] += results.loc[29, log]            
             averaged_results["IE"] += results.loc[449, log]
@@ -155,10 +161,95 @@ def make_report():
       #data_sheet = writer.sheets['Test_log']
       #data_sheet.set_column('A:', 40)
    messagebox.showinfo("Success", f"Report successfuly generated at: {test_report}")
+   return
+
+# Function to process yellow rig test data
+def make_yellow_report():
+   # Get entries
+   test_results = ent1.get()
+   logsheet = ent2.get()
+   test_report = ent3.get()
+   print("Yellow rig report preparing")
+   # Set headers and intialize an empty data frame
+   headers = ["Conditions", "Logs", "f [Hz]", "PR", "VR", f"SG [{chr(176)}C]", f"DG [{chr(176)}C]", f"SSH [{chr(176)}C]", "Duty [kW]", "Flow rate [m3/h]", "IE [%]", "VE [%]", "COP [-]"]
+   output_df = pd.DataFrame(columns = headers)
+   print(f"Report made from following files:\nF5 test log: {test_results}\nLogsheet: {logsheet}")
+   # Read the log file
+   results = pd.read_csv(test_results, encoding='latin1')
+   with open(logsheet,'r') as testlog:
+      for line in testlog:
+         single_logs = line.split("-")
+         # Initialize dictionary with average values
+         averaged_results = {"freq": 0, "PR": 1, "VR": 1.6, "Duty": 0, "Flow rate": 0, "SG": 0, "DG": 0, "SSH": 0, "IE": 0, "VE": 0, "COP":0}
+         print(f"Processing logs: {single_logs}")
+         for entry in single_logs:
+            log = entry.strip('\n')
+            # Get test data and add to dict
+            #Conditions = results.loc[3, log]
+            averaged_results["freq"] += float(results.loc[68, log])
+            averaged_results["PR"] = float(results.loc[23, log])
+            averaged_results["VR"] = float(results.loc[7, log])
+            averaged_results["Duty"] += float(results.loc[59, log])
+            averaged_results["Flow rate"] += float(results.loc[31, log])
+            averaged_results["SG"] += float(results.loc[13, log])
+            averaged_results["DG"] += float(results.loc[17, log])
+            averaged_results["SSH"] += float(results.loc[15, log])            
+            averaged_results["IE"] += float(results.loc[46, log])
+            averaged_results["VE"] += float(results.loc[45, log])
+            averaged_results["COP"] += float(results.loc[62, log])
+            
+         # Average data
+         for val in averaged_results:
+            if val != "Conditions" and val != "VR" and val != "PR":
+               averaged_results[val] = averaged_results[val] / len(single_logs)
+            
+         # Compile string for conditons
+         test_conditions = f"{round(averaged_results['SG'], 1)} / {round(averaged_results['DG'], 1)} @ {int(averaged_results['freq'] + 0.5)} Hz"
+
+         # Add results to output dataframe
+         output_df = output_df.append({"Conditions": test_conditions,
+                                        "Logs": line.strip('\n'),
+                                        "f [Hz]": int(averaged_results["freq"] + 0.5),
+                                        "PR": round(averaged_results['PR'], 1),
+                                        "VR": round(averaged_results['VR'], 1),
+                                        f"SG [{chr(176)}C]": averaged_results['SG'],
+                                        f"DG [{chr(176)}C]": averaged_results['DG'],
+                                        f"SSH [{chr(176)}C]": averaged_results["SSH"],
+                                        "Duty [kW]": averaged_results["Duty"],
+                                        "Flow rate [m3/h]": averaged_results["Flow rate"],
+                                        "IE [%]": averaged_results["IE"],
+                                        "VE [%]": averaged_results["VE"],
+                                        "COP [-]": averaged_results["COP"]}, ignore_index=True)
+
+         # Print results
+         print("Conditions: " + test_conditions)
+         print(f" SG = {round(averaged_results['SG'], 2)}\n DG = {round(averaged_results['DG'], 2)}\n SSH = {round(averaged_results['SSH'], 2)}\n Duty = {round(averaged_results['Duty'], 2)} kW\n Flow rate = {round(averaged_results['Flow rate'], 2)} m3/h\n IE = {round(averaged_results['IE'], 2)} %\n VE = {round(averaged_results['VE'], 2)} %\n COP = {round(averaged_results['COP'], 3)}\n")
+
+
+   with pd.ExcelWriter(test_report) as writer:    
+      # Write the dataframe into test report excel file
+      output_df.to_excel(writer, sheet_name='Summary', startrow=1, startcol=1, index=False)
+      #summary_sheet = writer.sheets['Summary']
+      #summary_sheet.set_column('B:O', 20)
+      # Add test log to the report
+      results.to_excel(writer, sheet_name='Test_log', startrow=0, startcol=0, index=False)
+      #data_sheet = writer.sheets['Test_log']
+      #data_sheet.set_column('A:', 40)
+   messagebox.showinfo("Success", f"Report successfuly generated at: {test_report}")
+   return
 
 # Close function
 def close_window(): 
     root.destroy()
+
+# Detect rig
+def make_report():
+   detected_rig = rig.get()
+   if detected_rig =="F5":
+      make_F5_report()
+   elif detected_rig == "yellow":
+      make_yellow_report()
+   return
 
 # Define button styling
 style = Style()
@@ -182,6 +273,10 @@ report_dest_btn.grid(row=3, column=3, padx=10, pady=10)
 # Define button to generate the report
 make_btn = Button(root, text ='Make report', style="C.TButton", command = lambda:make_report())
 make_btn.grid(row=5,column=1, padx=10, pady=10)
+
+# Temporary rig button
+#rig_btn = Button(root, text ='Detect Rig', style="C.TButton", command = lambda:detect_rig())
+#rig_btn.grid(row=5,column=2, padx=10, pady=10)
 
 # Define button to close/terminate the program
 cancel_btn = Button(root, text="Close", style="C.TButton", command = lambda:close_window())
